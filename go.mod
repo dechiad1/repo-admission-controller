6134a1b5f228aa6e1edd73283377a7eafb12f5fd
aserhat <serhata@gmail.com>
2019-10-18 16:35:11 -0400

    Unit Testing, Local Build and Small Scripts (#2)
    * Updating the deployment scripts and documentation
    
    * Added flag to controller to specify which repo to whitelist, updated build and deployment script
    
    * Updating Dockerfile to not use latest alpine image
    
    * Added safeRepo as a parameter to handleAdmission function for better testing, added some unit testing.

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/aserhat/repo-whitelist-controller
+
+go 1.12
+
+require (
+	k8s.io/api v0.0.0-20190927115716-5d581ce610b0
+	k8s.io/apimachinery v0.0.0-20191001195453-082230a5ffdd
+)
--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func handleAdmission(data []byte) *v1beta1.AdmissionReview {
+func handleAdmission(data []byte, safeRepo string) *v1beta1.AdmissionReview {
 	review := &v1beta1.AdmissionReview{}
 	err := json.Unmarshal(data, review)
 	if err != nil {
@@ -32,13 +32,15 @@ func handleAdmission(data []byte) *v1beta1.AdmissionReview {
 		Allowed: true,
 		Result: &metav1.Status{
 			Message: "Welcome!",
+			Reason:  "Image is coming from a safe repository",
 		},
 	}
+
 	for _, container := range pod.Spec.Containers {
-		if !strings.Contains(container.Image, flag.Lookup("saferepo").Value.(flag.Getter).Get().(string)+"/") {
+		if !strings.Contains(container.Image, safeRepo) {
 			reviewStatus.Allowed = false
 			reviewStatus.Result = &metav1.Status{
-				Reason: "can only pull registries in the defined repo",
+				Reason: "Can only pull images from the safe Repository",
 			}
 			log.Println("Blocking " + container.Image)
 		} else {
@@ -51,31 +53,31 @@ func handleAdmission(data []byte) *v1beta1.AdmissionReview {
 }
 
 func serve(w http.ResponseWriter, r *http.Request) {
-
-	log.Printf("request from %s, %s", r.Host, r.URL.Path)
+	log.Printf("Request from %s, %s", r.Host, r.URL.Path)
 	var bodyBytes []byte
 
 	if r.Body != nil {
 		bodyBytes, _ = ioutil.ReadAll(r.Body)
-
-		review := handleAdmission(bodyBytes)
+		safeRepo := flag.Lookup("saferepo").Value.(flag.Getter).Get().(string) + "/"
+		review := handleAdmission(bodyBytes, safeRepo)
 		if review == nil {
 			w.Write([]byte("No request body - body must contain valid kube pod creation spec"))
 		} else {
 			resp, err := json.Marshal(review)
 			if err != nil {
-				log.Println("marshalling admission review results into a response")
+				log.Println("Marshalling admission review results into a response")
 				panic(err)
 			}
 
 			if _, err := w.Write(resp); err != nil {
+				log.Println("Writing response failed")
 				log.Println("writing response failed")
 				panic(err)
 			}
 		}
 	} else {
 		//log this! body should always be present
-		log.Println("no body!")
+		log.Println("No body!")
 		w.Write([]byte("No request body - body must contain valid kube pod creation spec"))
 	}
 
@@ -83,13 +85,13 @@ func serve(w http.ResponseWriter, r *http.Request) {
 
 func parseFlags() {
 	var saferepo string
-	flag.StringVar(&saferepo, "saferepo", "registry1.lab-1.cloud.local", "Repository to white list")
+	flag.StringVar(&saferepo, "saferepo", "registry1.lab-1.cloud.local", "Repository to white list.")
 	flag.Parse()
 }
 
 func main() {
 	parseFlags()
-	log.Println("Conttroll starting and allowing images from registry: " + flag.Lookup("saferepo").Value.(flag.Getter).Get().(string))
+	log.Println("Controller starting and allowing images from registry: " + flag.Lookup("saferepo").Value.(flag.Getter).Get().(string))
 	http.HandleFunc("/", serve)
 
 	cert := "/etc/certs/tls.crt"
